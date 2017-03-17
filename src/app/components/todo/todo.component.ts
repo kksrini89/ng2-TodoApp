7704a67a0420d5008aa2a1250b33b445aa10546a
Srinivasan <kksrini89@gmail.com>
2017-03-17 23:08:35 +0530

    todo-add is being done

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-todo',
+  selector: 'todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
+  title: string = '';
+  description: string = '';
+  // todo: any = {
+
+  // };
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+  }
 }
