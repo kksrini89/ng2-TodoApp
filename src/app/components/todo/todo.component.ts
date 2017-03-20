79b85196be47932e776cfc4be9558090e60588a1
srinivasan <kksrini89@gmail.com>
2017-03-20 19:28:56 +0530

    Code cleanup is done.

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from "@angular/forms";
+import { FirebaseListObservable } from 'angularfire2';
+import { Firebaseauth } from "app/services/firebaseauth";
 
 @Component({
   selector: 'add-todo',
@@ -7,13 +9,17 @@ import { NgForm } from "@angular/forms";
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
-  todo: string = '';
-  constructor() { }
+  title: string = '';
+  description: string = '';
+  todos: FirebaseListObservable<any>;
+
+  constructor(private firebaseDb: Firebaseauth) { }
 
   ngOnInit() {
+    this.todos = this.firebaseDb.getTodos();
   }
 
   onSubmit(form: NgForm) {
-    // console.log(form);
+    console.log(form.value);
   }
 }
